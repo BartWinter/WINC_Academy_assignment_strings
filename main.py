21e7e2e382c90e1fb426d62643a7a328042523af
BartWinter <90457514+BartWinter@users.noreply.github.com>
2021-10-01 16:35:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+#Part 1
+scorer_1 = 'Gullit'
+scorer_2 = 'Van Basten'
+
+goal_0 = 32
+goal_1 = 54
+
+scorers = scorer_1 + ' ' + str(goal_0) + ',' + ' ' + scorer_2 + ' ' + str(goal_1)
+print(scorers)
+
+report = f'{scorer_1} scored in the {goal_0}nd minute' '\n' f'{scorer_2} scored in the {goal_1}th minute'
+print(report) 
+
+
+#Part 2
+player = 'Frank Rijkaard'
+space_position = player.find(' ')
+first_name = player[0:space_position]
+print(first_name)
+
+last_name = player[space_position+1:]
+last_name_len = len(last_name)
+print(last_name_len)
+
+
+name_short = player[0] + '.' + ' ' + last_name 
+print(name_short)
+
+chant = (first_name + '!' + ' ') * len(first_name)   
+print(chant)
+
+#print(good_chant)
+good_chant = chant[-1] != '' 
+print(good_chant)
